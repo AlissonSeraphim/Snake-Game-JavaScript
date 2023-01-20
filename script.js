285821c1ab9801225d4ab587cde98390de1c4a32
AlissonSeraphim <projetoseraphim@gmail.com>
2023-01-20 01:57:51 -0300

    substituindo o atributo descontinuado keyCode por key e alternando os valores do case

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ window.onload = function() {
 
   //  Chamada da função jogo a cada 100 milisegundos
   setInterval(jogo, 100)
+
+  // Controles
+  document.addEventListener('keydown', function(e){
+    switch(e.key) {
+      // Seta direita = 39
+      case 'ArrowRight':
+        velX = 1;
+        velY = 0;
+        break;
+      // Seta esquerda = 37
+      case 'ArrowLeft':
+        velX = -1;
+        velY = 0;
+        break;
+      // Seta cima = 38 (regra do eixo Y ao contrario da algebra para cima = - / para baixo = +)
+      case 'ArrowUp':
+        velX = 0;
+        velY = -1;
+        break;
+      // Seta baixo = 40
+      case 'ArrowDown':
+        velX = 0;
+        velY = 1;
+        break;
+    }
+
+  })
 }
 
 function jogo () {
@@ -24,12 +51,15 @@ function jogo () {
   //  Distância borda h, distancia borda v, largura, altura
   ctx.fillRect(0,0, canvas.width, canvas.height);
   
+  // Deslocamento da cobra
+  positionX += velX;
+  positionY += velY;
+
   // Posicionando a cobra
   snake.push({x: positionX, y: positionY});
 
   // Configuração da cobra
   ctx.fillStyle = '#00f102';
-  
   for (let index=0; index < snake.length; index +=1) {
     ctx.fillRect(snake[index].x*grid, snake[index].y*grid, grid-1, grid-1);
   }
